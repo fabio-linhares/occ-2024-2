6479dc9474e2fbc97ee0ed8bca7d4e2f43ab2bb8
Fábio Linhares <fabio.slinhares@prof.infnet.edu.br>
2025-05-01 08:36:02 -0300

    Remover versão antiga do Projeto_MercadoLivre

--- Projeto_MercadoLivre/src/algorithm/optimization_algorithm.cpp
+++
@@ -1,4 +0,0 @@
-#include "algorithm/optimization_algorithm.h"
-
-// Este arquivo pode ficar vazio pois a classe só tem métodos virtuais puros
-// Ou podemos adicionar implementações de utilitários comuns no futuro
--- Projeto_MercadoLivre/val/valida.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Compilar o validador com suporte para relatórios
-g++ -std=c++17 validador_completo.cpp -o validador_completo -lstdc++fs
-
-echo "Validador compilado com sucesso!"
-echo "Para executar: ./validador_completo"
-echo "Relatórios serão gerados em: relatorio_validacao.txt e relatorio_validacao.html"
